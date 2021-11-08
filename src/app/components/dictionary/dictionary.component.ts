0012935e4ad16a7e414b68ed3721ecc1e4a81406
Vladimir Bolshakov <andersdeath@yandex.ru>
2021-11-08 15:46:54 +0300

    view typings

--- src/app/components/dictionary/dictionary.component.ts
+++ src/app/components/dictionary/dictionary.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { DictionaryMock, Dictionary } from '../../data-lib';
 
+const TABLE_VIEW = 'TABLE_VIEW';
 
+const CARDS_VIEW = 'CARDS_VIEW';
 
 @Component({
   selector: 'app-dictionary',
@@ -9,11 +11,26 @@ import { DictionaryMock, Dictionary } from '../../data-lib';
   styleUrls: ['./dictionary.component.scss']
 })
 export class DictionaryComponent {
-  isTableView = true;
+  viewType: string = TABLE_VIEW;
   displayedColumns: string[] = ['subject', 'ru', 'en'];
+  storageName: string = 'viewType';
+  viewsTypes = {
+    cards: CARDS_VIEW,
+    table: TABLE_VIEW
+  }
+  version = 'v0.0.1';
+
   public dictionary: Dictionary = DictionaryMock;
 
-  isTableViewHandler(isTable: boolean) {
-    this.isTableView = isTable;
+  isTableViewHandler(val: string) {
+    this.viewType = val;
+    localStorage.setItem(this.storageName, val)
+  }
+
+  ngOnInit() {
+    const item = localStorage.getItem(this.storageName) || '';
+    if([CARDS_VIEW, TABLE_VIEW].includes(item)) {
+      this.viewType = item;
+    }
   }
 }
