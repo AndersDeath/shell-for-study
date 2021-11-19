f86b4d6da0f725ce49928584c902b707071d98df
Vladimir Bolshakov <andersdeath@yandex.ru>
2021-11-19 17:33:43 +0300

    update layout

--- src/app/components/dictionary/dictionary.component.ts
+++ src/app/components/dictionary/dictionary.component.ts
@@ -20,7 +20,7 @@ export class DictionaryComponent {
     table: TABLE_VIEW,
     fakeFlash: FAKE_FLASHCARDS_VIEW
   }
-  version = 'v0.0.8';
+  version = 'v0.0.9';
   fileUrl: SafeResourceUrl = '';
   fileName: string = 'collection.dict';
   fileReader = new FileReader();
