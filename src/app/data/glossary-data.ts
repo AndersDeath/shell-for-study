58ed50dfe602c68582ff7a214ad8667c366e1157
Vladimir Bolshakov <andersdeath@yandex.ru>
2021-12-17 16:05:15 +0300

    update glossary dict

--- src/app/data/glossary-data.ts
+++ src/app/data/glossary-data.ts
@@ -2,7 +2,7 @@ import { Dictionary } from "./data-lib";
 
 export const GlossaryData : Dictionary = {
   title: 'Glossary',
-  version: 2,
+  version: 3,
   sections: [
     {
       title: 'Programming',
@@ -54,6 +54,59 @@ Code is refactored for readability and maintainability. In particular, hard-code
 The cycle above is repeated for each new piece of functionality. Tests should be small and incremental, and commits made often. That way, if new code fails some tests, the programmer can simply undo or revert rather than debug excessively. When using external libraries, it is important not to write tests that are so small as to effectively test merely the library itself,[4] unless there is some reason to believe that the library is buggy or not feature-rich enough to serve all the needs of the software under development.
 
               `
+            },
+            {
+              subject: "Inheritance (object-oriented programming)",
+              ru: "",
+              en: `In object-oriented programming, **inheritance** is the mechanism of basing an object or class upon another object
+(prototype-based inheritance) or class (class-based inheritance), retaining similar implementation. Also defined
+as deriving new classes (sub classes) from existing ones such as super class or base class and then forming them into
+a hierarchy of classes. In most class-based object-oriented languages, an object created through inheritance, a
+"child object", acquires all the properties and behaviors of the "parent object" , with the exception of: constructors,
+destructor, overloaded operators and friend functions of the base class. Inheritance allows programmers to create classes
+that are built upon existing classes, to specify a new implementation while maintaining the same behaviors
+(realizing an interface), to reuse code and to independently extend original software via public classes and interfaces.
+The relationships of objects or classes through inheritance give rise to a directed acyclic graph.
+
+An **inherited** class is called a subclass of its parent class or super class. The term "inheritance"
+is loosely used for both class-based and prototype-based programming, but in narrow use the term is reserved for
+class-based programming (one class inherits from another), with the corresponding
+technique in prototype-based programming being instead called delegation (one object delegates to another).`
+            },
+            {
+              subject: "Polymorphism (computer science)",
+              ru: "",
+              en: `
+In programming languages and type theory, **polymorphism** is the provision of a single interface to entities of different types
+or the use of a single symbol to represent multiple different types.
+The concept is borrowed from a principle in biology where an organism
+or species can have many different forms or stages.
+
+The most commonly recognized major classes of polymorphism are:
+
+* Ad hoc polymorphism: defines a common interface for an arbitrary set of individually specified types.
+* Parametric polymorphism: when one or more types are not specified by name but by abstract symbols that can represent any type.
+* Subtyping (also called subtype polymorphism or inclusion polymorphism): when a name denotes instances of many different classes
+related by some common superclass.
+              `
+            },
+            {
+              subject: "Encapsulation (computer programming)",
+              ru: "",
+              en: `
+In object-oriented programming (OOP), **encapsulation** refers to the bundling of data with the
+methods that operate on that data, or the restricting of direct access to some of an object's components.
+Encapsulation is used to hide the values or state of a structured data object inside a class, preventing direct
+access to them by clients in a way that could expose hidden implementation details or violate state invariance
+maintained by the methods.
+
+Publicly accessible methods are generally provided in the class to access or modify the state more abstractly.
+In practice sometimes methods (so-called "getters" and "setters") are provided to access the values indirectly,
+but, although not necessarily a violation of abstract encapsulation, they are often considered a sign-post of
+potentially poor object-oriented programming (OOP) design practice (an Anti-pattern).
+
+This mechanism is not unique to OOP. Implementations of abstract data types, e.g., modules, offer a similar
+form of encapsulation. The similarity has been explained by programming language theorists in terms of existential types.`
             }
           ]
         },
