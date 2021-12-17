94ed2a8dce8fa783bcdcc41ac679db22ab44ea1c
Vladimir Bolshakov <andersdeath@yandex.ru>
2021-12-17 10:53:03 +0300

    update transition words dict

--- src/app/data/transition-words-data.ts
+++ src/app/data/transition-words-data.ts
@@ -2,7 +2,7 @@ import { Dictionary } from "./data-lib";
 
 export const TransitionWordsData : Dictionary = {
   title: 'Transition words',
-  version: 2,
+  version: 3,
   sections: [
     {
       title: 'Cause and effect',
@@ -84,5 +84,100 @@ export const TransitionWordsData : Dictionary = {
         }
       ]
     },
+    {
+      title: "Example",
+      groups: [
+        {
+          title: "Words",
+          subjects: [
+            {
+              subject: "For example",
+              ru: "Например, в отношении иммунизации установлены как краткосрочные, так и долгосрочные цели.",
+              en: "For example, immunization had both short- and long-term objectives."
+            },
+            {
+              subject: "for instance",
+              ru: "Для этой цели было бы целесообразно использовать, например, региональные тематические исследования.",
+              en: "Regional case studies, for instance, would be useful."
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Emphasis",
+      groups: [
+        {
+          title: "Words",
+          subjects: [
+            {
+              subject: "Above all",
+              ru: "Above all, Africa needs and deserves solidarity and respect.",
+              en: "Африка нуждается, прежде всего, в солидарности и уважении и заслуживает их."
+            },
+            {
+              subject: "most importantly",
+              ru: "Но самое главное: мы играем, чтобы посмотреть, как проигрывает Эми.",
+              en: "But most importantly, we play to watch Amy lose."
+            },
+            {
+              subject: "certainly",
+              ru: "Она, безусловно, не может покинуть Мальпертюи.",
+              en: "She certainly can't Leave Malpertuis."
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Similarity",
+      groups: [
+        {
+          title: "Words",
+          subjects: [
+            {
+              subject: "Likewise",
+              ru: "Он также приложил значительные усилия к агитации против призыва на военную службу в Америке.",
+              en: "Likewise, he expended considerable effort agitating against America's military draft."
+            },
+            {
+              subject: "similarly",
+              ru: "Аналогичным образом законы и постановления предусматривают защиту граждан от произвольного выселения.",
+              en: "Laws and directives similarly protect citizens from arbitrary eviction."
+            },
+            {
+              subject: "in the same vein",
+              ru: "Благодарю я и представителя Сингапура, выступавшего сразу после Китая и продолжавшего в том же духе/ключе.",
+              en: "I also thank the representative of Singapore, who spoke just after China and continued in the same vein."
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Summarize/conclude",
+      groups: [
+        {
+          title: "Words",
+          subjects: [
+            {
+              subject: "In conclusion",
+              ru: "В заключение я хотел бы пожелать всем успехов на этом новом, желанном пути.",
+              en: "In conclusion, I wish everyone every success in this new endeavour."
+            },
+            {
+              subject: "to sum up",
+              ru: "После всего вышесказанного что можно сказать, подводя итог уходящему столетию?",
+              en: "After all this, what can we say to sum up the passing of 100 years?"
+            },
+            {
+              subject: "in short",
+              ru: "Ну, короче говоря, мы никогда не знаем людей, которые окружают нас.",
+              en: "Well, in short, you never know the people around you."
+            }
+          ]
+        }
+      ]
+    }
   ]
 }
