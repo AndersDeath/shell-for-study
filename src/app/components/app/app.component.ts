5ee2bfb8dcb90dd5b863b252cef3653e0293d5b4
Vladimir Bolshakov <andersdeath@yandex.ru>
2021-12-17 11:35:08 +0300

    minor style fix

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -8,7 +8,7 @@ import { SidebarService } from 'src/app/services/sidebar.service';
 })
 export class AppComponent implements OnInit {
   public isOpened = false;
-  version = 'v0.5.1';
+  version = 'v0.5.2';
 
   constructor(
     public sidebar: SidebarService
