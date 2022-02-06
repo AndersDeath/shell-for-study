eebbb7d56332867dac229c71eb5cc6d17e05e759
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-02-06 17:39:44 +0300

    minor fix

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -20,7 +20,7 @@ function getSelectionText() {
 })
 export class AppComponent implements OnInit {
   public isOpened = false;
-  version = 'v0.8.0';
+  version = 'v0.8.1';
   showContextMenu = false;
   @ViewChild(MatMenuTrigger)
   trigger!: MatMenuTrigger;
