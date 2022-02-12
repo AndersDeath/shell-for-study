c6fc323a410dd247d953ccba79e756ad3ec13037
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-02-12 10:55:40 +0300

    minor layout fix for dashbpard

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -21,7 +21,7 @@ function getSelectionText() {
 })
 export class AppComponent implements OnInit {
   public isOpened = false;
-  version = 'v0.9.1';
+  version = 'v0.9.2';
   showContextMenu = false;
   @ViewChild(MatMenuTrigger)
   trigger!: MatMenuTrigger;
