9367e035a2b728b9c1fa803844ef4c4de8f27ba6
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-02-21 11:51:54 +0300

    update draft for registration form

--- src/app/modules/profile/components/user-registration/user-registration.component.ts
+++ src/app/modules/profile/components/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'sfs-user-registration',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-registration.component.scss']
 })
 export class UserRegistrationComponent implements OnInit {
-
-  constructor() { }
+  public registrationForm: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      password2: ['', [Validators.required]],
+    }, { validators: this.checkPasswords })
+  }
 
   ngOnInit(): void {
   }
 
+  sendFormData(form: any) {
+    console.log('send form data', form);
+  }
+
+  checkPasswords: ValidatorFn = (group: AbstractControl | any):  ValidationErrors | null => {
+    if(group !== null) {
+      let pass = group.get('password').value;
+      let confirmPass = group.get('password2').value
+      return pass === confirmPass ? null : { notSame: true }
+    }
+    return null
+  }
 }
