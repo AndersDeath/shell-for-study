a4737e28c7c89d2afee07c4d2aea7235e621802b
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-02-23 12:32:20 +0300

    increase the version

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -19,7 +19,7 @@ enum ContextLinks {
 })
 export class AppComponent implements OnInit {
   public isOpened = false;
-  public version = 'v0.9.8';
+  public version = 'v0.9.9';
   public showContextMenu = false;
   @ViewChild(MatMenuTrigger)
   public trigger!: MatMenuTrigger;
