6f76db324f17fc7edaea3ddfb7e516a814506a66
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-02-24 19:39:27 +0300

    ipdate restore password form

--- src/app/modules/profile/components/user-restore-password/user-restore-password.component.ts
+++ src/app/modules/profile/components/user-restore-password/user-restore-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { UserApiService } from '../../services/user-api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sfs-user-restore-password',
@@ -10,6 +11,7 @@ import { UserApiService } from '../../services/user-api.service';
 export class UserRestorePasswordComponent implements OnInit {
 
   public restorePasswordForm: FormGroup;
+  private subs: Subscription[] = []
   constructor(
     public fb: FormBuilder,
     private api: UserApiService
@@ -24,6 +26,15 @@ export class UserRestorePasswordComponent implements OnInit {
 
   sendFormData(form: any) {
     console.log('send form data', form);
+    const sub = this.api.registration(form.value);
+    // this.subs.push(sub);
   }
 
+  ngOnDestroy() {
+    if(this.subs.length > 0) {
+      this.subs.forEach((s: Subscription) => {
+        s.unsubscribe();
+      })
+    }
+  }
 }
