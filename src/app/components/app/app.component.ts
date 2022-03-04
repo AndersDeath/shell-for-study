27c262b45454ee52e9947b585d39b1393397a4a1
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-03-04 20:30:28 +0300

    increase version

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -12,7 +12,7 @@ interface TranslateModel {
 const EN = 'en';
 const RU = 'ru';
 
-const version = 'v0.9.25';
+const version = 'v0.9.26';
 
 const EN_TRANSLATION: TranslateModel = {
   download: 'Download'
