7f14509ca0752d32288a1cb58b5b90c8deb67e5c
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-03-06 18:00:53 +0300

    convert user restore password form to presentational component

--- src/app/modules/profile/components/user-restore-password/user-restore-password.component.ts
+++ src/app/modules/profile/components/user-restore-password/user-restore-password.component.ts
@@ -1,41 +1,40 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { UserApiService } from '../../../../services/user-api.service';
-import { Subscription } from 'rxjs';
-import { EventEmitter } from 'stream';
+
+export class UserRestorPasswordModel {
+  public email: string = ''
+  constructor(email?: string) {
+    this.email = email || '';
+  }
+}
 
 @Component({
   selector: 'sfs-user-restore-password',
   templateUrl: './user-restore-password.component.html',
   styleUrls: ['./user-restore-password.component.scss']
 })
-export class UserRestorePasswordComponent implements OnInit {
-  // @Output() submit: EventEmitter = new EventEmitter();
+export class UserRestorePasswordComponent {
+
+  @Input() set formData(data: any) {
+    this.restorePasswordForm.setValue({
+      email: data.email
+    })
+  }
+  @Output() formDataEmitter = new EventEmitter<UserRestorPasswordModel>();
+
   public restorePasswordForm: FormGroup;
-  private subs: Subscription[] = []
   constructor(
-    public fb: FormBuilder,
-    private api: UserApiService
-  ) {
+    public fb: FormBuilder
+      ) {
     this.restorePasswordForm = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
     })
   }
 
-  ngOnInit(): void {
-  }
-
   sendFormData(form: any) {
     console.log('send form data', form);
-    const sub = this.api.registration(form.value);
-    // this.subs.push(sub);
-  }
-
-  ngOnDestroy() {
-    if(this.subs.length > 0) {
-      this.subs.forEach((s: Subscription) => {
-        s.unsubscribe();
-      })
-    }
+    this.formDataEmitter.emit({
+      email: form.value.email
+    });
   }
 }
