e24f124531aaa373d4d434fbb3ea17b5a585191d
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-03-11 09:12:54 +0300

    update user-settings form

--- src/app/modules/profile/components/user-settings/user-settings.component.ts
+++ src/app/modules/profile/components/user-settings/user-settings.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+interface UserProfileSettingsModel {
+  name: string;
+  password: string;
+}
 @Component({
   selector: 'sfs-user-settings',
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.scss']
 })
-export class UserSettingsComponent implements OnInit {
+export class UserSettingsComponent {
+  @Input() set formData(data: any) {
+    this.loginForm.setValue({
+      name: data.name,
+      password: data.password
+    })
+  }
+  @Output() formDataEmitter = new EventEmitter<UserProfileSettingsModel>();
 
-  constructor() { }
+  public loginForm: FormGroup;
+  constructor(
+    public fb: FormBuilder
+  ) {
+    this.loginForm = this.fb.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
 
-  ngOnInit(): void {
+  sendFormData(form: FormGroup) {
+    this.formDataEmitter.emit({
+      name: form.value.name,
+      password: form.value.password
+    });
   }
 
 }
