f55c16a73a0c47fb03e4e94ef0e5b78b4b08e7ef
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-03-18 12:30:29 +0300

    update

--- src/app/data/ru-uk-phrasebook-data.ts
+++ src/app/data/ru-uk-phrasebook-data.ts
@@ -3,5 +3,20 @@ import { Dictionary } from "./data-lib";
 export const RuUkPhrasebookData : Dictionary = {
   title: 'Russian-Ukranian Phrasebook',
   version: 1,
-  sections: []
+  sections: [
+    {
+      title: "Общение",
+      groups: [
+        {
+          title: "Приветствие / Biтання",
+          subjects: [
+            {
+              subject: 'Доброе утро!',
+              uk: 'Доброго ранку!'
+            }
+          ]
+        }
+      ]
+    }
+  ]
 }
