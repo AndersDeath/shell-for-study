4a6211ef1a5148d0db9c79542d3e48c11f929921
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-03-24 11:12:37 +0300

    update bibliography

--- src/app/pages/bibliography-page/bibliography-page.component.ts
+++ src/app/pages/bibliography-page/bibliography-page.component.ts
@@ -11,13 +11,24 @@ export class BibliographyPageComponent implements OnInit {
 
   public items: SFSMenuItem[] = createSFSMenuData(true);
   public title: string = "Bibliography";
-  public linksList: any[] = [
+  public linksList: {
+    url: string;
+    header: string;
+    title: string;
+    description: string;
+  }[] = [
     {
       url: "https://lingust.ru/",
       header: "lingust.ru",
       title: "Изучение иностранных языков с нуля!",
       description: "Сайт с оцифрованными и преобразованными в уроки самоучителями различных языков"
     },
+    {
+      url: "https://www.bbc.co.uk/learningenglish/",
+      header: "BBC Learning English",
+      title: "Learning english, inspiring language learning since 1943",
+      description: "Learn English with these free learning English videos and materials from BBC Learning English. This site will help you learn English and improve your pronunciation, grammar and vocabulary knowledge."
+    }
   ]
 
   constructor(
