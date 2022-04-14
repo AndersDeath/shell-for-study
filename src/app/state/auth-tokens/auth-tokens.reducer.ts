87fb52310b9e321ba2d6f82fd54749761f5412db
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-04-14 17:06:27 +0300

    update reducer and data version

--- src/app/state/auth-tokens/auth-tokens.reducer.ts
+++ src/app/state/auth-tokens/auth-tokens.reducer.ts
@@ -1,16 +1,23 @@
-
-
 import { createReducer, on } from '@ngrx/store';
 import { Tokens } from 'sfs-data-model';
 import { update } from './auth-tokens.actions';
 
+interface SFStore {
+  tokens: Tokens;
+  profile: {};
+}
 
-export const initialState: Tokens = new Tokens({
-  access_token: '',
-  refresh_token: ''
-});
+export const initialState: SFStore = {
+  profile: {},
+  tokens: new Tokens({
+    access_token: '',
+    refresh_token: '',
+  }),
+};
 
 export const authTokensReducer = createReducer(
   initialState,
-  on(update, (state, tokens) => { return {...state, ...tokens}})
+  on(update, (state, tokens) => {
+    return { ...state, ...tokens };
+  })
 );
