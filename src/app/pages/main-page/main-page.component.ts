67b92244888219277dd6c546e9518485fa81e1c4
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-05-10 15:34:41 +0300

    update

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,7 +1,9 @@
+import { Subscription } from 'rxjs';
+import { UserApiService } from 'src/app/services/user-api/user-api.service';
 import { UserLoginModel } from 'sfs-data-model';
 import { authLogin } from 'src/app/state/auth/auth.actions';
 import { Store } from '@ngrx/store';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarService } from '../../services/sidebar/sidebar.service';
 
@@ -10,22 +12,24 @@ import { SidebarService } from '../../services/sidebar/sidebar.service';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
   public title: string = "Main page";
   public isLoginShow = false;
   public isRegisterShow = false;
   public isRestorePassword = false;
 
-  public data2: UserLoginModel = new UserLoginModel({
+  public loginData: UserLoginModel = new UserLoginModel({
     email: 'test@test.test',
     password: '12',
   });
 
+  private subscriptions: Subscription[] = []
 
   constructor(
     public sidebar: SidebarService,
     private router: Router,
-    private store: Store
+    private store: Store,
+    private api: UserApiService
 
     ) { }
 
@@ -39,12 +43,16 @@ export class MainPageComponent implements OnInit {
     this.sidebar.toggle();
   }
 
-  public formDataEmitter2(data: any) {
-    this.store.dispatch(authLogin(data));
+  public loginEmitter(data: any) {
+    const sub = this.api.login(data).subscribe((e) => {
+      console.log(e);
+    })
+    this.subscriptions.push(sub);
+    // this.store.dispatch(authLogin(data));
 
     // TODO: remove it in prod
-    localStorage.setItem('isLogin','true')
-    this.router.navigate(['dashboard']);
+    // localStorage.setItem('isLogin','true')
+    // this.router.navigate(['dashboard']);
   }
 
   showLogin() {
@@ -63,6 +71,11 @@ export class MainPageComponent implements OnInit {
     this.isLoginShow = true;
     this.isRegisterShow = false;
     this.isRestorePassword = true;
+  }
 
+  ngOnDestroy() {
+    this.subscriptions.forEach((e: Subscription) => {
+      e.unsubscribe();
+    })
   }
 }
