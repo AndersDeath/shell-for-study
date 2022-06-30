c197481bfe821ee828b240ced2773c088f58488a
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-06-30 12:25:36 +0400

    update

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { selectProfile } from './../../state/auth.selectors';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from 'sfs-data-model';
-import { UserProfileSerivce } from 'src/app/services/user-profile/user-profile.service';
 import { SidebarService } from '../../services/sidebar/sidebar.service';
 
 @Component({
@@ -8,21 +10,28 @@ import { SidebarService } from '../../services/sidebar/sidebar.service';
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.scss']
 })
-export class UserPageComponent implements OnInit {
+export class UserPageComponent implements OnInit, OnDestroy {
   public title: string = "Demo User";
   public user = new User({});
-
+  private sub: Subscription = new Subscription;
   constructor(
     public sidebar: SidebarService,
-    private profile: UserProfileSerivce
+    private store: Store
   ) { }
 
   ngOnInit(): void {
-    this.user = this.profile.get();
+    this.sub = this.store.select(selectProfile).subscribe((e: User) => {
+      console.log(e);
+      this.user = e;
+    });
   }
 
   toggleSidebar() {
     this.sidebar.toggle();
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
   }
 
 }
